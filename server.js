1b0ae247379da043c409db23269de5767e91a64d
TeodorH <teodor.hornfeldt07@gmail.com>
2026-05-06 10:17:31 +0200

    Added movies db and GET movies. Also added npm run dev to package.json

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-const express = require("express");
+const express = require('express');
+const sqlite3 = require("sqlite3");
 const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+const db = new sqlite3.Database("./movies.db");
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    release_year INTEGER NOT NULL,
+    genre TEXT,
+    rating INTEGER,
+    watched BOOLEAN DEFAULT FALSE)
+`);
 
 app.get("/", (req, res) => {
 res.send("Hello from my server!");
+});
+
+app.get("/movies", (req, res) => {
+  db.all("SELECT * FROM movies", (error, rows) => {
+    if (rows.length === 0) {
+      res.status(200).json({"message": "No movies found"});
+      return;
+    }
+    res.json(rows);
+  });
 });
 
 app.listen(3000, () => {
